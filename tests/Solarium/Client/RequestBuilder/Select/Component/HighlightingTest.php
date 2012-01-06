39256a0748bc3f9ef0e059827e7e95f97ea184f1
Bas de Nooijer <github@raspberry.nl>
2012-01-06 10:09:06 +0100

    Added hl.q and hl.phraseLimit support to highlighting component

--- tests/Solarium/Client/RequestBuilder/Select/Component/HighlightingTest.php
+++ tests/Solarium/Client/RequestBuilder/Select/Component/HighlightingTest.php
@@ -70,6 +70,8 @@ class Solarium_Client_RequestBuilder_Select_Component_HighlightingTest extends P
         $component->setRegexSlop(1.3);
         $component->setRegexPattern('mypattern');
         $component->setMaxAnalyzedChars(100);
+        $component->setQuery('text:myvalue');
+        $component->setPhraseLimit(40);
 
         $request = $builder->build($component, $request);
 
@@ -95,6 +97,8 @@ class Solarium_Client_RequestBuilder_Select_Component_HighlightingTest extends P
                 'hl.highlightMultiTerm' => 'true',
                 'hl.regex.slop' => 1.3,
                 'hl.regex.pattern' => 'mypattern',
+                'hl.q' => 'text:myvalue',
+                'hl.phraseLimit' => 40,
                 'f.fieldB.hl.snippets' => 3,
                 'f.fieldB.hl.fragsize' => 25,
                 'f.fieldB.hl.mergeContiguous' => 'true',
