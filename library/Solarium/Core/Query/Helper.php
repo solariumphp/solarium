9ba2c676284c349a7b92bc7722ef0284ec0a7edf
Garanzha Dmitriy <dimonsoft@gmail.com>
2013-09-02 10:49:06 +0300

    Fix escaping

--- library/Solarium/Core/Query/Helper.php
+++ library/Solarium/Core/Query/Helper.php
@@ -103,7 +103,7 @@ class Helper
      */
     public function escapeTerm($input)
     {
-        $pattern = '/(\+|-|&&|\|\||!|\(|\)|\{|}|\[|]|\^|"|~|\*|\?|:|\/|\\\|AND|OR|NO)/';
+        $pattern = '/(\+|-|&&|\|\||!|\(|\)|\{|}|\[|]|\^|"|~|\*|\?|:|\/|\\\|\bAND\b|\bOR\b|\bNO\b)/';
 
         return preg_replace($pattern, '\\\$1', $input);
     }
