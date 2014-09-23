8bae941570a07fe9f0074a09e9feceaa00ee00d3
Tobias Liebig <work@etobi.de>
2014-09-23 14:27:47 +0200

    Support HTTPS Solr server
    Fixes: #267

--- tests/Solarium/Tests/Core/Client/EndpointTest.php
+++ tests/Solarium/Tests/Core/Client/EndpointTest.php
@@ -48,6 +48,7 @@ class EndpointTest extends \PHPUnit_Framework_TestCase
     public function testConfigMode()
     {
         $options = array(
+            'scheme' => 'http',
             'host' => '192.168.0.1',
             'port' => 123,
             'path' => '/mysolr/',
@@ -99,11 +100,24 @@ class EndpointTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(7, $this->endpoint->getTimeout());
     }
 
+    public function testSetAndGetScheme()
+    {
+        $this->endpoint->setScheme('https');
+        $this->assertEquals('https', $this->endpoint->getScheme());
+    }
+
     public function testGetBaseUri()
     {
         $this->endpoint->setHost('myserver')->setPath('/mypath')->setPort(123);
 
         $this->assertEquals('http://myserver:123/mypath/', $this->endpoint->getBaseUri());
+    }
+
+    public function testGetBaseUriWithHttps()
+    {
+        $this->endpoint->setScheme('https')->setHost('myserver')->setPath('/mypath')->setPort(123);
+
+        $this->assertEquals('https://myserver:123/mypath/', $this->endpoint->getBaseUri());
     }
 
     public function testGetBaseUriWithCore()
