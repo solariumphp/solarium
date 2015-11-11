0ea2603f16603dd7c15964035640cf865a0aa317
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-11-11 10:13:40 +0100

    Include vendors inside the PHAR.
    Since Solarium has dependencies, this is more interesting to include
    vendors (aka dependencies) inside the PHAR file.
    
    Thus we include the `library/` directory with the `vendor/` directory.

--- phar/stub.php
+++ phar/stub.php
@@ -34,7 +34,7 @@
  */
 
 Phar::mapPhar("solarium.phar");
-require_once 'phar://solarium.phar/Autoloader.php';
+require_once 'phar://solarium.phar/library/Solarium/Autoloader.php';
 Solarium\Autoloader::register();
 
 if ('cli' === php_sapi_name() && basename(__FILE__) === basename($_SERVER['argv'][0]) && isset($_SERVER['argv'][1])) {
