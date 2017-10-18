85636358c4846da99bf191c9a8cf3c25825c9812
Ema Panz <thePanz@users.noreply.github.com>
2017-10-18 13:19:10 +0200

    Add Pagination example
    Add a pagination example, see #521

--- examples/1.2-basic-select.php
+++ examples/1.2-basic-select.php
@@ -9,6 +9,15 @@ $client = new Solarium\Client($config);
 // get a select query instance
 $query = $client->createQuery($client::QUERY_SELECT);
 
+// Pagination example
+$resulsPerPage = 15;
+$currentPage = 1;
+        
+// Set the number of results to return
+$query->setRows($resultsPerPage);
+// Set the 0-based result to start from, taking into account pagination
+$query->setStart(($currentPage - 1) * $resultsPerPage);
+
 // this executes the query and returns the result
 $resultset = $client->execute($query);
 
