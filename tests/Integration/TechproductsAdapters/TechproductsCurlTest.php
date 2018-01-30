f92c91ca9ae61d19b91b97f74a830a8caa433042
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2018-01-30 10:13:22 +0100

    raise timeout in integration tests for curl adapter (#576)
    The default timeout of solarium of 5s seems to be too aggressive on travis and causes random test failures. Set it to the PHP default of 13s.

--- tests/Integration/TechproductsAdapters/TechproductsCurlTest.php
+++ tests/Integration/TechproductsAdapters/TechproductsCurlTest.php
@@ -9,4 +9,11 @@ use Solarium\Tests\Integration\AbstractTechproductsTest;
  */
 class TechproductsCurlTest extends AbstractTechproductsTest
 {
+    public function setUp()
+    {
+        parent::setUp();
+        // The default timeout of solarium of 5s seems to be too aggressive on travis and causes random test failures.
+        // Set it to the PHP default of 13s.
+        $this->client->getEndpoint()->setTimeout(CURLOPT_TIMEOUT);
+    }
 }
