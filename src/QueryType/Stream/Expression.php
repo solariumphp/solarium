37872d182e077fa14c8c72f3c324a3c32c3900a4
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2018-03-15 15:05:08 +0100

    coding style

--- src/QueryType/Stream/Expression.php
+++ src/QueryType/Stream/Expression.php
@@ -19,15 +19,15 @@ class Expression
      */
     public function __call(string $name, array $arguments)
     {
-        return $name.'('.implode(', ', array_filter($arguments, function($value) {
+        return $name.'('.implode(', ', array_filter($arguments, function ($value) {
             if (is_array($value) || (is_object($value) && !method_exists($value, '__toString'))) {
-              throw new InvalidArgumentException('An expression argument must be a scalar value or an object that provides a __toString() method.');
+                throw new InvalidArgumentException('An expression argument must be a scalar value or an object that provides a __toString() method.');
             }
             if (is_string($value)) {
-              $value = trim($value);
+                $value = trim($value);
             }
             // Eliminate empty string arguments.
-            return ($value !== '');
+            return $value !== '';
         })).')';
     }
 }
