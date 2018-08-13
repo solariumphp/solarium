9bc3d788599db849205ea0ab2314e26ca43161da
Jürgen Enge <je4@users.noreply.github.com>
2018-08-13 17:21:32 +0200

    Update FacetSet.php

--- src/Component/RequestBuilder/FacetSet.php
+++ src/Component/RequestBuilder/FacetSet.php
@@ -126,20 +126,11 @@ class FacetSet extends RequestBuilder implements ComponentRequestBuilderInterfac
 
         if( $compatible )
         {
-			$request->addParam("f.$field.facet.limit", $facet->getLimit());
-			$request->addParam("f.$field.facet.sort", $facet->getSort());
-			$request->addParam("f.$field.facet.prefix", $facet->getPrefix());
-			$request->addParam("f.$field.facet.contains", $facet->getContains());
-			$request->addParam("f.$field.facet.contains.ignoreCase", $facet->getContainsIgnoreCase());
-			$request->addParam("f.$field.facet.offset", $facet->getOffset());
-			$request->addParam("f.$field.facet.mincount", $facet->getMinCount());
-			$request->addParam("f.$field.facet.missing", $facet->getMissing());
-			$request->addParam("f.$field.facet.method", $facet->getMethod());
-			$local_param = ['key' => $facet->getKey(), 'ex' => $facet->getExcludes()];
+		$local_param = ['key' => $facet->getKey(), 'ex' => $facet->getExcludes()];
         }
         else
         {
-			$local_param = ['key' => $facet->getKey(), 'ex' => $facet->getExcludes(), 'facet.limit' => $facet->getLimit(), 'facet.sort' => $facet->getSort(), 'facet.prefix' => $facet->getPrefix(), 'facet.contains' => $facet->getContains(), 'facet.contains.ignoreCase' => $facet->getContainsIgnoreCase(), 'facet.offset' => $facet->getOffset(), 'facet.mincount' => $facet->getMinCount(), 'facet.missing' => $facet->getMissing(), 'facet.method' => $facet->getMethod()];
+		$local_param = ['key' => $facet->getKey(), 'ex' => $facet->getExcludes(), 'facet.limit' => $facet->getLimit(), 'facet.sort' => $facet->getSort(), 'facet.prefix' => $facet->getPrefix(), 'facet.contains' => $facet->getContains(), 'facet.contains.ignoreCase' => $facet->getContainsIgnoreCase(), 'facet.offset' => $facet->getOffset(), 'facet.mincount' => $facet->getMinCount(), 'facet.missing' => $facet->getMissing(), 'facet.method' => $facet->getMethod()];
         }
         $request->addParam(
             'facet.field',
@@ -148,6 +139,18 @@ class FacetSet extends RequestBuilder implements ComponentRequestBuilderInterfac
                 $local_param
             )
         );
+        if( $compatible )
+        {
+		$request->addParam("f.$field.facet.limit", $facet->getLimit());
+		$request->addParam("f.$field.facet.sort", $facet->getSort());
+		$request->addParam("f.$field.facet.prefix", $facet->getPrefix());
+		$request->addParam("f.$field.facet.contains", $facet->getContains());
+		$request->addParam("f.$field.facet.contains.ignoreCase", $facet->getContainsIgnoreCase());
+		$request->addParam("f.$field.facet.offset", $facet->getOffset());
+		$request->addParam("f.$field.facet.mincount", $facet->getMinCount());
+		$request->addParam("f.$field.facet.missing", $facet->getMissing());
+		$request->addParam("f.$field.facet.method", $facet->getMethod());
+        }
     }
 
     /**
