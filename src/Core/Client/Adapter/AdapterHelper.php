c4fb7b7ba2958f246929745208aa67a87ebec71b
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2019-02-06 13:25:31 +0100

    Backward compatibility: getBaseUri() now throws an UnexpectedValueException and we don't send a request. In previous version we sent the request which resulted in HttpException.

--- src/Core/Client/Adapter/AdapterHelper.php
+++ src/Core/Client/Adapter/AdapterHelper.php
@@ -4,22 +4,39 @@ namespace Solarium\Core\Client\Adapter;
 
 use Solarium\Core\Client\Endpoint;
 use Solarium\Core\Client\Request;
+use Solarium\Exception\HttpException;
+use Solarium\Exception\UnexpectedValueException;
 
 /**
  * Helper class for shared adapter functionality.
  */
 class AdapterHelper
 {
+    /**
+     * @param Request  $request
+     * @param Endpoint $endpoint
+     *
+     * @return string
+     *
+     * @throws HttpException
+     */
     public static function buildUri(Request $request, Endpoint $endpoint): string
     {
-        if (Request::API_V2 == $request->getApi()) {
-            $baseUri = $endpoint->getV2BaseUri();
-        } elseif ($request->getIsServerRequest()) {
-            $baseUri = $endpoint->getV1BaseUri();
-        } else {
-            $baseUri = $endpoint->getBaseUri();
+        try {
+            if (Request::API_V2 == $request->getApi()) {
+                $baseUri = $endpoint->getV2BaseUri();
+            } elseif ($request->getIsServerRequest()) {
+                $baseUri = $endpoint->getV1BaseUri();
+            } else {
+                $baseUri = $endpoint->getBaseUri();
+            }
+            return $baseUri . $request->getUri();
         }
-        return $baseUri.$request->getUri();
+        catch (UnexpectedValueException $e) {
+            // Backward compatibility: getBaseUri() now throws an UnexpectedValueException and we don't send a request.
+            // In previous version we sent the request which resulted in HttpException.
+            throw new HttpException($e->getMessage(), 404, $e->getTraceAsString());
+        }
     }
 
     /**
