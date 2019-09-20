969f861123dc899bb2fb3344bee5c1219d4ac3d8
Szabolcs Zsoldos <zsoldos@materom.ro>
2019-09-20 18:23:31 +0300

    Update Pivot.php
    Provide essential pivot methods: setLimit() and getLimit()

--- src/Component/Facet/Pivot.php
+++ src/Component/Facet/Pivot.php
@@ -36,6 +36,27 @@ class Pivot extends AbstractFacet implements ExcludeTagsInterface
     {
         return FacetSetInterface::FACET_PIVOT;
     }
+    
+    	/**
+	 * Set the facet limit.
+	 *
+	 * @param int $limit
+	 *
+	 * @return self Provides fluent interface
+	 */
+	public function setLimit($limit): self
+	{
+		return $this->setOption('limit', $limit);
+	}
+	/**
+	 * Get the facet limit.
+	 *
+	 * @return int
+	 */
+	public function getLimit(): ?int
+	{
+		return $this->getOption('limit');
+	}
 
     /**
      * Set the facet mincount.
