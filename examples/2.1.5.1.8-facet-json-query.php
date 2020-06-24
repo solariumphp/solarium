13a3dcccaa22cdd3e60ce46bf13785f69ff3cd1c
Mark Rogerson <mark.rogerson@bodleian.ox.ac.uk>
2020-06-24 17:34:33 +0100

    Resolves solariumphp/solarium#819

---
+++ examples/2.1.5.1.8-facet-json-query.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once(__DIR__.'/init.php');
+htmlHeader();
+
+// create a client instance
+$client = new Solarium\Client($adapter, $eventDispatcher, $config);
+
+// get a select query instance
+$query = $client->createSelect();
+
+// get the facetset component
+$facetSet = $query->getFacetSet();
+
+// create a json query instance and set options
+$inStockFacet = new Solarium\Component\Facet\JsonQuery(['local_key' => 'stock_query', 'query' => 'inStock:true']);
+
+// add json query instance to the facetSet
+$facetSet->addFacet($inStockFacet);
+
+// this executes the query and returns the result
+$resultset = $client->select($query);
+
+// display the total number of documents found by solr
+echo 'NumFound: '.$resultset->getNumFound();
+
+// JsonQuery returns a FacetSet, not Buckets like a jsonTerms facet
+$stock_query = $resultset->getFacetSet()->getFacet('stock_query');
+
+// the facet count is returned as a 'count' sub-facet
+$count = $stock_query->getFacet('count')->getValue();
+
+echo '<hr/>Facet "inStock" count : ' . $count;
+
+// show documents using the resultset iterator
+foreach ($resultset as $document) {
+
+    echo '<hr/><table>';
+    echo '<tr><th>id</th><td>' . $document->id . '</td></tr>';
+    echo '<tr><th>name</th><td>' . $document->name . '</td></tr>';
+    echo '<tr><th>price</th><td>' . $document->price . '</td></tr>';
+    echo '</table>';
+}
+
+htmlFooter();
