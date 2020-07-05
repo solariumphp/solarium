30a088e78ed0fa251cb39fb6b3d56127a194ef22
wickedOne <wicliff.wolda@gmail.com>
2020-07-05 12:50:58 +0200

    added (composite) comparison and value classes

---
+++ src/Builder/CompositeComparison.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Solarium\Builder;
+
+use Solarium\Exception\RuntimeException;
+
+/**
+ * Composite Expression.
+ *
+ * @author wicliff <wicliff.wolda@gmail.com>
+ */
+class CompositeComparison implements ExpressionInterface
+{
+    public const TYPE_AND = 'AND';
+
+    public const TYPE_OR = 'OR';
+
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @var \Solarium\Builder\Comparison[]
+     */
+    private $comparisons = [];
+
+    /**
+     * @param string $type
+     * @param array  $comparisons
+     *
+     * @throws \Solarium\Exception\RuntimeException
+     */
+    public function __construct($type, array $comparisons)
+    {
+        $this->type = $type;
+
+        foreach ($comparisons as $expr) {
+            if ($expr instanceof Value) {
+                throw new RuntimeException('Values are not supported expressions as children of and/or expressions.');
+            }
+            if (!($expr instanceof ExpressionInterface)) {
+                throw new RuntimeException('No expression given to CompositeExpression.');
+            }
+
+            $this->comparisons[] = $expr;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return \Solarium\Builder\Comparison[]
+     */
+    public function getComparisons(): array
+    {
+        return $this->comparisons;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function visit(AbstractExpressionVisitor $visitor)
+    {
+        return $visitor->walkCompositeExpression($this);
+    }
+}
