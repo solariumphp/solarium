c7c7eab4deb5a56e0ccf0b35c8cbfbda221ce423
thomascorthals <thomas_corthals@hotmail.com>
2020-12-30 01:32:03 +0100

    @todo for distributed search referencing #908

--- tests/Integration/AbstractTechproductsTest.php
+++ tests/Integration/AbstractTechproductsTest.php
@@ -1567,6 +1567,7 @@ abstract class AbstractTechproductsTest extends TestCase
 
         // in Solr 7, atomic updates of child documents aren't possible
         // in SolrCloud mode, this fails more often with "Async exception during distributed update" than it succeeds
+        // @todo get this sorted for distributed search when #908 is resolved
         if (8 <= self::$solrVersion && $this instanceof AbstractServerTest) {
             // atomic update: removing all child documents
             $doc = $update->createDocument();
