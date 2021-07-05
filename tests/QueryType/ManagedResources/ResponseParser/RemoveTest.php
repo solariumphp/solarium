db70be9bb84982e3008f2e2d944c9d068e4f1b85
thomascorthals <thomas_corthals@hotmail.com>
2021-07-05 20:53:47 +0200

    Backward compatible ResponseParser for COMMAND_REMOVE

---
+++ tests/QueryType/ManagedResources/ResponseParser/RemoveTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Solarium\Tests\QueryType\ManagedResources\ResponseParser;
+
+use PHPUnit\Framework\TestCase;
+use Solarium\Core\Client\Response;
+use Solarium\QueryType\ManagedResources\Query\Stopwords as Query;
+use Solarium\QueryType\ManagedResources\ResponseParser\Remove as ResponseParser;
+use Solarium\QueryType\ManagedResources\Result\Command as CommandResult;
+
+class RemoveTest extends TestCase
+{
+    public function testParseSuccessfulResult()
+    {
+        $query = new Query();
+        $result = new CommandResult($query, new Response('', ['HTTP/1.1 200 OK']));
+
+        $parser = new ResponseParser();
+
+        $parsed = $parser->parse($result);
+
+        $this->assertTrue($parsed['wasSuccessful']);
+        $this->assertSame('OK', $parsed['statusMessage']);
+    }
+
+    public function testParseUnsuccessfulResult()
+    {
+        $query = new Query();
+        $result = new CommandResult($query, new Response('', ['HTTP/1.1 404 Not Found']));
+
+        $parser = new ResponseParser();
+
+        $parsed = $parser->parse($result);
+
+        $this->assertFalse($parsed['wasSuccessful']);
+        $this->assertSame('Not Found', $parsed['statusMessage']);
+    }
+}
