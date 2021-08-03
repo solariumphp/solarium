1d0db7414161035cbc79a1a548467ec9f312e174
Markus Kalkbrenner <git@kalki.de>
2021-08-03 17:36:23 +0200

    fixed assertion

--- tests/Integration/AbstractCloudTest.php
+++ tests/Integration/AbstractCloudTest.php
@@ -102,10 +102,10 @@ abstract class AbstractCloudTest extends AbstractTechproductsTest
         $this->assertTrue($result->getWasSuccessful());
         $this->assertContains('_default', $result->getConfigSets());
         $this->assertContains('techproducts', $result->getConfigSets());
-        $this->assertNotContains('techproducts.COPY', $result->getConfigSets());
+        $this->assertNotContains('copy-of-techproducts', $result->getConfigSets());
 
         $action = $configsetsQuery->createCreate();
-        $action->setName('techproducts.COPY')->setBaseConfigSet('techproducts');
+        $action->setName('copy-of-techproducts')->setBaseConfigSet('techproducts');
         $configsetsQuery->setAction($action);
         $result = self::$client->configsets($configsetsQuery);
         $this->assertTrue($result->getWasSuccessful());
@@ -116,6 +116,6 @@ abstract class AbstractCloudTest extends AbstractTechproductsTest
         $this->assertTrue($result->getWasSuccessful());
         $this->assertContains('_default', $result->getConfigSets());
         $this->assertContains('techproducts', $result->getConfigSets());
-        $this->assertContains('techproducts.COPY', $result->getConfigSets());
+        $this->assertContains('copy-of-techproducts', $result->getConfigSets());
     }
 }
