2afadd17fe0f537c8144785fa1ee78f0e4d7084b
thomascorthals <thomas_corthals@hotmail.com>
2022-07-22 12:47:44 +0200

    Use official regex to check for semantic version string

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -19,7 +19,12 @@ class ClientTest extends TestCase
     {
         $installedVersion = InstalledVersions::getPrettyVersion('solarium/solarium');
 
-        if ('dev-master' === $installedVersion) {
+        // @see https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+        $semverRegex =
+            '/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/'
+        ;
+
+        if (!preg_match($semverRegex, $installedVersion)) {
             self::assertSame($installedVersion, Client::getVersion());
             self::markTestSkipped(sprintf('Testing on %s, skipping tests against version tag %s.', $installedVersion, self::$versionTag));
         }
