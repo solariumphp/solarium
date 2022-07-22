73f848a4d03e14c0c1c940efbb6e79e6699b588a
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2022-07-22 11:14:25 +0200

    ExceptionInterface must extends Throwable (#1024)

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -14,6 +14,6 @@ namespace Solarium\Exception;
  *
  * For background info see http://ralphschindler.com/2010/09/15/exception-best-practices-in-php-5-3
  */
-interface ExceptionInterface
+interface ExceptionInterface extends \Throwable
 {
 }
