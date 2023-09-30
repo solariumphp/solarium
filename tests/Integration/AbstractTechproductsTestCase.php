solariumphp/solarium

commit 7afd75fd18d208ab696e5b74be5072dcfdb46186

: self typehints (#1087)

src/Component/QueryTrait.php
@@ -24,7 +24,7 @@ trait QueryTrait
      *
      * @return self Provides fluent interface
      */
-    public function setQuery(string $query, array $bind = null): QueryInterface
+    public function setQuery(string $query, array $bind = null): self
     {
         if (null !== $bind) {
             $helper = $this->getHelper();

commit eabb93743282696204a1b6822260bf46dde415f1

Fix BC issue introduced in #1087

src/Component/QueryTrait.php
@@ -24,7 +24,7 @@ trait QueryTrait
      *
      * @return self Provides fluent interface
      */
-    public function setQuery(string $query, array $bind = null): self
+    public function setQuery(string $query, array $bind = null): QueryInterface
     {
         if (null !== $bind) {
             $helper = $this->getHelper();

tests/Integration/AbstractTechproductsTestCase.php
@@ -5,6 +5,7 @@ namespace Solarium\Tests\Integration;
 use PHPUnit\Framework\TestCase;
 use Solarium\Component\ComponentAwareQueryInterface;
 use Solarium\Component\Highlighting\Highlighting;
+use Solarium\Component\QueryInterface;
 use Solarium\Component\QueryTraits\GroupingTrait;
 use Solarium\Component\QueryTraits\TermsTrait;
 use Solarium\Component\Result\Grouping\FieldGroup;
@@ -57,6 +58,9 @@ use Solarium\QueryType\Update\Query\Query as UpdateQuery;
 use Solarium\QueryType\Update\RequestBuilder\Xml as XmlUpdateRequestBuilder;
 use Solarium\Support\Utility;
 use Solarium\Tests\Integration\Plugin\EventTimer;
+use Solarium\Tests\Integration\Query\CustomQueryInterfaceQuery;
+use Solarium\Tests\Integration\Query\CustomSelfQuery;
+use Solarium\Tests\Integration\Query\CustomStaticQuery;
 use Symfony\Contracts\EventDispatcher\Event;
 
 abstract class AbstractTechproductsTestCase extends TestCase
@@ -5065,6 +5069,32 @@ abstract class AbstractTechproductsTestCase extends TestCase
         }
 
         self::$client->removePlugin('eventtimer');
+    }
+
+    /**
+     * Test the various return types that are valid for custom query classes that
+     * override the {@see \Solarium\Component\QueryTrait::setQuery()} method.
+     *
+     * If this test throws a fatal error, the return type of the parent might no
+     * longer be backward compatible with existing code that overrides it.
+     *
+     * @see https://github.com/solariumphp/solarium/issues/1097
+     *
+     * @dataProvider customQueryClassProvider
+     */
+    public function testCustomQueryClassSetQueryReturnType(string $queryClass)
+    {
+        $query = new $queryClass();
+        $this->assertInstanceOf(QueryInterface::class, $query->setQuery('*:*'));
+    }
+
+    public function customQueryClassProvider(): array
+    {
+        return [
+            [CustomStaticQuery::class],
+            [CustomSelfQuery::class],
+            [CustomQueryInterfaceQuery::class],
+        ];
     }
 }
 

tests/Integration/Query/CustomQueryInterfaceQuery.php
@@ -1,0 +1,20 @@
+<?php
+
+namespace Solarium\Tests\Integration\Query;
+
+use Solarium\Component\QueryInterface;
+use Solarium\QueryType\Select\Query\Query as SelectQuery;
+
+/**
+ * Custom query that overrides the setQuery() method with a QueryInterface return type.
+ */
+class CustomQueryInterfaceQuery extends SelectQuery
+{
+    /**
+     * @return self Provides fluent interface
+     */
+    public function setQuery(string $query, array $bind = null): QueryInterface
+    {
+        return parent::setQuery($query, $bind);
+    }
+}

tests/Integration/Query/CustomSelfQuery.php
@@ -1,0 +1,19 @@
+<?php
+
+namespace Solarium\Tests\Integration\Query;
+
+use Solarium\QueryType\Select\Query\Query as SelectQuery;
+
+/**
+ * Custom query that overrides the setQuery() method with a self return type.
+ */
+class CustomSelfQuery extends SelectQuery
+{
+    /**
+     * @return self Provides fluent interface
+     */
+    public function setQuery(string $query, array $bind = null): self
+    {
+        return parent::setQuery($query, $bind);
+    }
+}

tests/Integration/Query/CustomStaticQuery.php
@@ -1,0 +1,19 @@
+<?php
+
+namespace Solarium\Tests\Integration\Query;
+
+use Solarium\QueryType\Select\Query\Query as SelectQuery;
+
+/**
+ * Custom query that overrides the setQuery() method with a static return type.
+ */
+class CustomStaticQuery extends SelectQuery
+{
+    /**
+     * @return static Provides fluent interface
+     */
+    public function setQuery(string $query, array $bind = null): static
+    {
+        return parent::setQuery($query, $bind);
+    }
+}