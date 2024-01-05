058006ad002574c025664201ba12e387b0090041
thomascorthals <thomas_corthals@hotmail.com>
2024-01-05 23:39:53 +0100

    Additional test coverage

--- tests/Plugin/NoWaitForResponseRequestTest.php
+++ tests/Plugin/NoWaitForResponseRequestTest.php
@@ -3,12 +3,16 @@
 namespace Solarium\Tests\Plugin;
 
 use PHPUnit\Framework\TestCase;
+use Solarium\Client;
 use Solarium\Core\Client\Adapter\AdapterInterface;
+use Solarium\Core\Client\Adapter\Curl;
+use Solarium\Core\Client\Adapter\Http;
 use Solarium\Core\Client\Adapter\TimeoutAwareInterface;
-use Solarium\Core\Client\Client;
+use Solarium\Core\Client\Endpoint;
 use Solarium\Core\Client\Request;
 use Solarium\Core\Event\Events;
 use Solarium\Core\Event\PreExecuteRequest as PreExecuteRequestEvent;
+use Solarium\Exception\HttpException;
 use Solarium\Plugin\NoWaitForResponseRequest;
 use Solarium\QueryType\Select\Query\Query;
 use Solarium\Tests\Integration\TestClientFactory;
@@ -16,12 +20,12 @@ use Solarium\Tests\Integration\TestClientFactory;
 class NoWaitForResponseRequestTest extends TestCase
 {
     /**
-     * @var \Solarium\Plugin\NoWaitForResponseRequest
+     * @var NoWaitForResponseRequest
      */
     protected $plugin;
 
     /**
-     * @var \Solarium\Core\Client\Adapter\Curl
+     * @var Client
      */
     protected $client;
 
@@ -95,6 +99,85 @@ class NoWaitForResponseRequestTest extends TestCase
         // settings changed within the event subscriber.
         $this->assertSame(TimeoutAwareInterface::DEFAULT_TIMEOUT, $this->client->getAdapter()->getTimeout());
         $this->assertTrue($this->client->getAdapter()->getOption('return_transfer'));
+    }
+
+    public function testSetFastTimeout()
+    {
+        $observer = new class() extends Http {
+            public $newTimeout;
+
+            public function setTimeout(int $timeoutInSeconds): self
+            {
+                if (!isset($this->newTimeout)) {
+                    $this->newTimeout = $timeoutInSeconds;
+                }
+
+                return parent::setTimeout($timeoutInSeconds);
+            }
+        };
+        $this->client->setAdapter($observer);
+
+        $requestOutput = $this->client->createRequest($this->query);
+        $endpoint = $this->client->getEndpoint();
+        $endpoint->setCore('my_core');
+        $event = new PreExecuteRequestEvent($requestOutput, $endpoint);
+        $this->plugin->preExecuteRequest($event);
+
+        $this->assertSame(TimeoutAwareInterface::FAST_TIMEOUT, $observer->newTimeout);
+    }
+
+    public function testSetFastTimeoutWithConnectionTimeout()
+    {
+        $observer = new class() extends Curl {
+            public $newTimeout;
+
+            public function setTimeout(int $timeoutInSeconds): self
+            {
+                if (!isset($this->newTimeout)) {
+                    $this->newTimeout = $timeoutInSeconds;
+                }
+
+                return parent::setTimeout($timeoutInSeconds);
+            }
+        };
+        $observer->setConnectionTimeout(1);
+        $this->client->setAdapter($observer);
+
+        $requestOutput = $this->client->createRequest($this->query);
+        $endpoint = $this->client->getEndpoint();
+        $endpoint->setCore('my_core');
+        $event = new PreExecuteRequestEvent($requestOutput, $endpoint);
+        $this->plugin->preExecuteRequest($event);
+
+        $this->assertSame(1 + TimeoutAwareInterface::FAST_TIMEOUT, $observer->newTimeout);
+    }
+
+    public function testUnrelatedHttpExceptionIsRethrown()
+    {
+        $requestOutput = $this->client->createRequest($this->query);
+        $endpoint = $this->client->getEndpoint();
+        $event = new PreExecuteRequestEvent($requestOutput, $endpoint);
+
+        // thrown by AdapterHelper::buildUri() because we didn't set a core or collection on the endpoint
+        $this->expectException(HttpException::class);
+        $this->expectExceptionCode(404);
+        $this->plugin->preExecuteRequest($event);
+    }
+
+    public function testUnrelatedExceptionIsRethrown()
+    {
+        $requestOutput = $this->client->createRequest($this->query);
+        $endpoint = $this->client->getEndpoint();
+        $event = new class($requestOutput, $endpoint) extends PreExecuteRequestEvent {
+            public function getEndpoint(): Endpoint
+            {
+                throw new \RuntimeException('', 42);
+            }
+        };
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionCode(42);
+        $this->plugin->preExecuteRequest($event);
     }
 
     public function testPluginIntegration()
