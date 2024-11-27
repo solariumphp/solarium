f79265e6b4b909ac78136945062b500ed2c9a2e0
Marko Kaznovac <kaznovac@users.noreply.github.com>
2024-11-27 15:50:12 +0100

    Remove dead link from ExceptionInterface.php (#1137)
    the url is last seen 2019-04-01 as per wayback machine
    while article is quite good and informative there's a little benefit of keeping it's reference in today's code doc
    
    https://web.archive.org/web/20190401130536/http://ralphschindler.com/2010/09/15/exception-best-practices-in-php-5-3
    
    Co-authored-by: Markus Kalkbrenner <git@kalki.de>

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -11,8 +11,6 @@ namespace Solarium\Exception;
 
 /**
  * Marker Interface for Solarium exceptions.
- *
- * For background info see http://ralphschindler.com/2010/09/15/exception-best-practices-in-php-5-3
  */
 interface ExceptionInterface extends \Throwable
 {
